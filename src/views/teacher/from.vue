8bf9c705a6ce6f8c00ac114e44257c6913a44d0f
tom_glb <goodMorning_pro@atguigu.com>
2020-01-17 21:54:01 +0800

    头像上传控件！

--- src/views/teacher/from.vue
+++ src/views/teacher/from.vue
@@ -25,7 +25,29 @@
       </el-form-item>
 
       <!-- 讲师头像：TODO -->
-
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+        <!--
+          v-show：是否显示上传组件
+          :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+          :url：后台上传的url地址
+          @close：关闭上传组件
+          @crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/oss/file/upload'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+      </el-form-item>
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -35,6 +57,8 @@
 
 <script>
   import teacher from '@/api/edu/teacher'
+  import ImageCropper from '@/components/ImageCropper'
+  import PanThumb from '@/components/PanThumb'
 
   const defaultForm = {
     name: '',
@@ -42,14 +66,18 @@
     level: '',
     career: '',
     intro: '',
-    avatar: ''
+    avatar: process.env.OSS_PATH + '/avatar/default.jpg'
   }
 
   export default {
+    components: { ImageCropper, PanThumb },
     data() {
       return {
         teacher: defaultForm,
-        saveBtnDisabled: false // 保存按钮是否禁用
+        saveBtnDisabled: false, // 保存按钮是否禁用
+        BASE_API: process.env.BASE_API, // 接口API地址
+        imagecropperShow: false, // 是否显示上传组件
+        imagecropperKey: 0 // 上传组件id
       }
     },
     watch: {
@@ -130,6 +158,22 @@
             message: '保存失败'
           })
         })
+      },
+      // 上传成功后的回调函数
+      cropSuccess(data) {
+        alert(1)
+        console.log(data)
+        this.imagecropperShow = false
+        this.teacher.avatar = data.url
+        // 上传成功后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+        this.imagecropperKey = this.imagecropperKey + 1
+      },
+
+      // 关闭上传组件
+      close() {
+        this.imagecropperShow = false
+        // 上传失败后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+        this.imagecropperKey = this.imagecropperKey + 1
       }
 
     }
