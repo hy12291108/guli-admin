3a667d32c1621afd8ae3b77d3772808c7b599bb7
hy12291108 <hy950908>
2020-02-14 13:42:15 +0800

    统计分析

--- src/views/statistics/chart.vue
+++ src/views/statistics/chart.vue
@@ -17,24 +17,25 @@
           v-model="searchObj.begin"
           type="date"
           placeholder="选择开始日期"
-          value-format="yyyy-MM-dd" />
+          value-format="yyyy-MM-dd"/>
       </el-form-item>
       <el-form-item>
         <el-date-picker
           v-model="searchObj.end"
           type="date"
           placeholder="选择截止日期"
-          value-format="yyyy-MM-dd" />
+          value-format="yyyy-MM-dd"/>
       </el-form-item>
       <el-button
         :disabled="btnDisabled"
         type="primary"
         icon="el-icon-search"
-        @click="showChart()">查询</el-button>
+        @click="showChart()">查询
+      </el-button>
     </el-form>
 
     <div class="chart-container">
-      <div id="chart" class="chart" style="height:500px;width:100%" />
+      <div id="chart" class="chart" style="height:500px;width:100%"/>
     </div>
   </div>
 </template>
@@ -42,6 +43,7 @@
 <script>
 
   import echarts from 'echarts'
+  import daily from '@/api/statistics/daily'
 
   export default {
     data() {
@@ -62,12 +64,36 @@
       showChart() {
         this.initChartData()
 
-        this.setChart()
+        //this.setChart()
       },
 
       // 准备图表数据
       initChartData() {
+        daily.showChart(this.searchObj).then(response => {
+          // 数据
+          this.yData = response.data.dataList
 
+          // 横轴时间
+          this.xData = response.data.dateList
+
+          // 当前统计类别
+          switch (this.searchObj.type) {
+            case 'register_num':
+              this.title = '学员注册数统计'
+              break
+            case 'login_num':
+              this.title = '学员登录数统计'
+              break
+            case 'video_view_num':
+              this.title = '课程播放数统计'
+              break
+            case 'course_num':
+              this.title = '每日课程数统计'
+              break
+          }
+
+          this.setChart()
+        })
       },
 
       // 设置图标参数
@@ -81,7 +107,7 @@
           // x轴是类目轴（离散数据）,必须通过data设置类目数据
           xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: this.xData//-------绑定数据
           },
           // y轴是数据轴（连续数据）
           yAxis: {
@@ -90,10 +116,43 @@
           // 系列列表。每个系列通过 type 决定自己的图表类型
           series: [{
             // 系列中的数据内容数组
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            data: this.yData,//-------绑定数据
             // 折线图
             type: 'line'
-          }]
+          }],
+          title: {
+            text: this.title
+          },
+          tooltip: {
+            trigger: 'axis'
+          },
+          dataZoom: [{
+            show: true,
+            height: 30,
+            xAxisIndex: [
+              0
+            ],
+            bottom: 30,
+            start: 10,
+            end: 80,
+            handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+            handleSize: '110%',
+            handleStyle: {
+              color: '#d3dee5'
+
+            },
+            textStyle: {
+              color: '#fff'
+            },
+            borderColor: '#90979c'
+          },
+            {
+              type: 'inside',
+              show: true,
+              height: 15,
+              start: 1,
+              end: 35
+            }]
         }
 
         this.chart.setOption(option)
